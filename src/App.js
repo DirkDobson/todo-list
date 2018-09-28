949dc4dba3122f6f33f40dd81c2b2d7864d8eb6a
Dirk Dobson <dirkvancouver@live.com>
2018-09-28 09:42:17 -0600

    app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Form, Header, Container, List, Input, Segment } from 'semantic-ui-react';
 
-class App extends Component {
+class App extends React.Component {
+  state = { name: '', todos: [] }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, todos } = this.state;
+    this.setState({ todos: [name, ...todos], name: '' })
+  }
+
   render() {
+    const { name, todos } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Segment textAlign="center">
+          <Header as="h3" textAlign="center">Todo List</Header>
+          <Form onSubmit={this.handleSubmit}>
+            <Input 
+              required
+              value={name}
+              onChange={ e => this.setState({ name: e.target.value }) }
+            />
+          </Form>
+          <List>
+            { todos.map( (t,i) => <List.Item key={i}>{t}</List.Item> )}
+          </List>
+        </Segment>
+      </Container>
     );
   }
 }
